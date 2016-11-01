20c70c53a79955237add64e8cfe08faac24041af
lastjuly <lastjuly0731@gmail.com>
2016-11-02 04:52:07 +0900

    re lastjuly

--- rc/wrc/wfrc/Form1.Designer.cs
+++ rc/wrc/wfrc/Form1.Designer.cs
@@ -28,8 +28,6 @@
     /// </summary>
     private void InitializeComponent()
     {
-<<<<<<< HEAD
-            this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
@@ -38,17 +36,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(235, 11);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(37, 21);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "확인";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
@@ -59,15 +48,15 @@
             "103",
             "104",
             "105"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 38);
+            this.comboBox1.Location = new System.Drawing.Point(14, 35);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(75, 20);
+            this.comboBox1.Size = new System.Drawing.Size(87, 20);
             this.comboBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Location = new System.Drawing.Point(12, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 12);
             this.label1.TabIndex = 2;
@@ -82,15 +71,15 @@
             "103",
             "104",
             "105"});
-            this.comboBox2.Location = new System.Drawing.Point(120, 38);
+            this.comboBox2.Location = new System.Drawing.Point(140, 35);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(75, 20);
+            this.comboBox2.Size = new System.Drawing.Size(87, 20);
             this.comboBox2.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(88, 42);
+            this.label2.Location = new System.Drawing.Point(103, 39);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 5;
@@ -99,7 +88,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(199, 43);
+            this.label3.Location = new System.Drawing.Point(232, 40);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 12);
             this.label3.TabIndex = 6;
@@ -107,36 +96,46 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(234, 37);
+            this.button2.Location = new System.Drawing.Point(273, 34);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(40, 23);
+            this.button2.Size = new System.Drawing.Size(47, 21);
             this.button2.TabIndex = 7;
             this.button2.Text = "이동";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 64);
+            this.textBox2.Location = new System.Drawing.Point(15, 61);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(262, 303);
+            this.textBox2.Size = new System.Drawing.Size(305, 306);
             this.textBox2.TabIndex = 8;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(95, 11);
+            this.textBox1.Location = new System.Drawing.Point(111, 10);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(133, 21);
+            this.textBox1.Size = new System.Drawing.Size(154, 21);
             this.textBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "접속";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 379);
+            this.ClientSize = new System.Drawing.Size(329, 379);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
@@ -150,37 +149,12 @@
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
-=======
-      this.button1 = new System.Windows.Forms.Button();
-      this.SuspendLayout();
-      // 
-      // button1
-      // 
-      this.button1.Location = new System.Drawing.Point(34, 40);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
-      this.button1.TabIndex = 0;
-      this.button1.Text = "button1";
-      this.button1.UseVisualStyleBackColor = true;
-      this.button1.Click += new System.EventHandler(this.button1_Click);
-      // 
-      // Form1
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(509, 411);
-      this.Controls.Add(this.button1);
-      this.Name = "Form1";
-      this.Text = "Form1";
-      this.ResumeLayout(false);
->>>>>>> 3eea0482bddeff1ef7836422afe18cde27caeb19
 
     }
 
     #endregion
 
-    private System.Windows.Forms.Button button1;
-<<<<<<< HEAD
+
     private System.Windows.Forms.ComboBox comboBox1;
     private System.Windows.Forms.Label label1;
     private System.Windows.Forms.ComboBox comboBox2;
@@ -189,8 +163,8 @@
     private System.Windows.Forms.Button button2;
     private System.Windows.Forms.TextBox textBox2;
     private System.Windows.Forms.TextBox textBox1;
-=======
->>>>>>> 3eea0482bddeff1ef7836422afe18cde27caeb19
+    private System.Windows.Forms.Button button1;
+
   }
 }
 
--- rc/wrc/wfrc/Form1.cs
+++ rc/wrc/wfrc/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,47 +11,46 @@ using System.Windows.Forms;
 
 namespace wfrc
 {
-  public partial class Form1 : Form
-  {
-    dplnet dn;
-
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
-<<<<<<< HEAD
-      textBox1.Text = "---.---.-.-";
-      dn = new dlpnet();
-=======
+        dplnet dn;
 
-      dn = new dplnet();
->>>>>>> 3eea0482bddeff1ef7836422afe18cde27caeb19
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = "---.---.-.-";
+            dn = new dplnet();
+
+
+            dn = new dplnet();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text + " 접속";
+            byte [] ip = Encoding.Default.GetBytes("127.0.0.1");
+            dn.DNAOpen(ip, 20000, 0x00);
+
+        }
+
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            textBox2.Text = comboBox1.Text + " 에서 " + comboBox2.Text + " 으로 이동 중";
+        }
+
     }
-
-    private void button1_Click(object sender, EventArgs e)
-    {
-        textBox2.Text = textBox1.Text + " 접속";
-        //byte [] ip = Encoding.Default.GetBytes("127.0.0.1");
-        //dn.DNAOpen(ip, 20000, 0x00);
-      
-    }
-<<<<<<< HEAD
-
-    private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-    {
-
-    }
-
-    private void textBox2_TextChanged(object sender, EventArgs e)
-    {
-       
-    }
-
-    private void button2_Click(object sender, EventArgs e)
-    {
-
-        textBox2.Text = comboBox1.Text + comboBox2.Text;
-    }
-=======
->>>>>>> 3eea0482bddeff1ef7836422afe18cde27caeb19
-  }
 }
