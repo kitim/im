3f7fd07c1ba60e3f86ec760c1fe1eee9ca045aa3
chan2222 <chanyeong2588@gmail.com>
2016-11-02 01:29:00 +0900

    min soo arduino

--- rc/wificonn.ino
+++ rc/wificonn.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+#include "ESP8266.h"  //위에꺼랑 이거랑 같이쓰면오류안남
 #define BAUD_RATE     115200
 #define UART_WRITE         0
 #define UART_READ          1
@@ -11,6 +12,13 @@ const char UART_COMMAND[][UART_BUF_SZ] =
   "AT+CWMODE=1\r\n",     //1
   "AT+CWJAP_CUR=\"DALS\",\"DAL@D510\"", ///2
 };
+int UART_COMMAND_DELAY[] =
+{
+  100,
+  1000,
+  20000,
+};
+
 
 SoftwareSerial srl(6, 7); // RX, TX
 
@@ -23,7 +31,7 @@ void setup()
   // set the data rate for the SoftwareSerial port
   srl.begin(BAUD_RATE);
 
-  //while (!srl.available()) ;
+  while (!srl.available()) ;
   memset(buf, 0, sizeof(char)*UART_BUF_CNT*UART_BUF_SZ);
   Serial.begin(9600);
 }
@@ -34,7 +42,7 @@ void bufClear(int idx)
   memset(buf[idx], 0, sizeof(char)*UART_BUF_SZ);
 }
 
-int checkAck()
+int checkAck(int* cmd)
 {
   int e = -1;
   int idx = 0;
@@ -42,49 +50,52 @@ int checkAck()
   {
     buf[UART_READ][uart_index] = srl.read();
 
-    if ( (uart_index>0) && (buf[UART_READ][uart_index-1]=='O') && (buf[UART_READ][uart_index]=='K') )
+    if (  (uart_index>0) && (buf[UART_READ][uart_index-1]=='O') && (buf[UART_READ][uart_index]=='K') )
     {
       Serial.write("checkAck SUCCESS \r\n");
       bufClear(UART_READ);
       e = idx;
+      *cmd = 1;
       break;
     }
+    if ( (uart_index>0) && (buf[UART_READ][uart_index-1]=='\r') && (buf[UART_READ][uart_index]=='\n')  )
+    {
+      Serial.write("checkAck SUCCESS 2\r\n");
+      bufClear(UART_READ);
+      *cmd = 1;
+      break;
+    }
+    
   }
 
   if ( e < 0 )
   {
     bufClear(UART_READ);
-    Serial.write("checkAck FAIL \r\n");
+    //Serial.write("checkAck FAIL \r\n");
   }
   return e;
 }
 
-int readUart(int count)
+int readUart(int count, int* cmd)
 {
   int e = -1;
   int idx = 0;
   for ( idx = 0 ; idx<count ; idx++ )
   {
-    if ( checkAck() >= 0 )
+    e = checkAck(cmd);
+    if ( e > 0 )
     {
-      Serial.write("readUart SUCCESS \r\n");
-      e = idx;
       break;
     }
-  }
-
-  if ( e < 0 )
-  {
-    Serial.write("readUart FAIL \r\n");
   }
   return e;  
 }
 
-int writeAT(int idx, int count)
+int writeAT(int idx, int count, int* cmd)
 {
   int e = -1;
-  srl.write(UART_COMMAND[idx]);
-  if ( readUart(count) >= 0 )
+  if ( *cmd > 0 ) srl.write(UART_COMMAND[idx], strlen(UART_COMMAND[idx]));
+  if ( readUart(count, cmd) > 0 )
   {
     e = idx;
   }
@@ -93,12 +104,14 @@ int writeAT(int idx, int count)
 
 void loop()
 {
-  int e = 0;
+  int e = -1;
+  int c = 1;  //// 1:Command Enable,   0:Command Disable
   uart_command_index = 0;
+  
   while ( 1 )
   {
-    e = writeAT(uart_command_index, 100);
-    if ( e>=0 )
+    e = writeAT(uart_command_index, UART_COMMAND_DELAY[uart_command_index], &c);
+    if ( e>0 )
     {
       if ( uart_command_index == 0 )
       {
@@ -114,11 +127,12 @@ void loop()
         break;
       }
     }
+    
   }
 
   while (1 )
   {
-    writeAT(0, 100);
+    //writeAT(0, 1000, &c);
     delay(2000);
   }
 
