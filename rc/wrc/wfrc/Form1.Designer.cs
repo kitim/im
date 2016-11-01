3a7252828f2dd1764230b4b84b11352d9aed7cc9
lastjuly <lastjuly0731@gmail.com>
2016-11-01 23:21:40 +0900

    re heesoo

--- rc/wrc/wfrc/Form1.Designer.cs
+++ rc/wrc/wfrc/Form1.Designer.cs
@@ -118,6 +118,7 @@
             this.textBox2.Location = new System.Drawing.Point(12, 64);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox2.Size = new System.Drawing.Size(262, 303);
             this.textBox2.TabIndex = 8;
